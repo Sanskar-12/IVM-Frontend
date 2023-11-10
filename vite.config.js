d6456f5c4c914da9293aaec920e30a093b6bfcdf
Sanskar-12 <sanskarv2004@gmail.com>
2023-11-11 00:00:26 +0530

    frontend

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   proxy: {
-  //     "/api/v1": "https://ivm-backend.onrender.com/api/v1",
-  //   },
-  // },
+  server: {
+    proxy: {
+      "/api/v1": "https://ivm-backend.onrender.com/api/v1",
+    },
+  },
 
   plugins: [react()],
 });
